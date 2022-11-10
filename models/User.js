bfa3ee634953b9adf86ee68b6c84b390a6433a29
Zachary Warnes <zacharywarnes@gmail.com>
2022-11-09 18:14:41 -0800

    updating User model with required content. creating virtual for friendCount

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const {Schema, model} = require('mongoose');
+
+const userSchema = newSchema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [ /^[a-z0-9._%+-]{1,64}@(?:[a-z0-9-]{1,63}\.){1,125}[a-z]{2,63}$/i, 'Please provide a valid email address' ]   
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model ('User', userSchema);
+
+module.exports = User;
