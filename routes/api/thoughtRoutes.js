03cc17b417e372c3cc9ec4b9a78e54c93fa1cf01
Zachary Warnes <zacharywarnes@gmail.com>
2022-11-13 16:15:48 -0800

    reloading npm packages and exporting thoughts router

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -25,11 +25,5 @@ router.route('/:id/reactions').post(createReaction);
 // /api/:id/reactions/reactionsId
 router.route('/:id/reactions/reactionsId').delete(deleteReaction);
 
-//GET all thoughts
-//GET a single thought by _id
-//POST to create a new thought and push to user's thoughts array field
-//PUT to update a thought by its _id
-//DELETE to remove a thought by its _id
+module.exports = router;
 
-//POST to create a reaction stored in a single thought's reactions array field
-//DELETE to pull and remove a reaction by reactionId value
